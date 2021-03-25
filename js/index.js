690e6b294457e37586440c50cb5e5fb7bdd590d0
melissamahon3 <melissamahon3@gmail.com>
2021-03-24 22:46:17 -0400

    19-homework

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log("Your index.js file is loaded correctly!");
+
+// Hover Color for Nav Bar
+
+$(function() {
+    $('.showlink').hover(function() {
+        $(this).addClass('hovered');
+    },
+    function(){
+        $(this).removeClass('hovered');
+    });
+});
